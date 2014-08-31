c8713a9067773378c6948d902f5bbd56fb9891e6
arthur1234 <kidumaikido@gmail.com>
2014-08-31 18:59:23 +0300

    added tag Set Text

--- javascript/main.js
+++ javascript/main.js
@@ -1,51 +1,64 @@
-/* my js */
 
-var changCSS = 0;
+
+var changeColor_Flag = 0;    // 1: change background Color
+                             // 2: change border Color
+                             // 3: change text Color
 
 $(function() {
     // rgb(255, 140, 60);
 
+    // open dialog window to change color
     $("#dialog").dialog({
         autoOpen: false,
         resizable: false
     });
 
 
-
-    $("#slider-range-max").slider({
+    // set default border width
+    $("#slider-range-border").slider({
         range: "max",
         min: 0,
         max: 10,
         value: 1,
         slide: function(event, ui) {
-            $("#amount").val(ui.value);
+            $("#amount-border").val(ui.value);
         }
     });
-    $("#amount").val($("#slider-range-max").slider("value"));
 
-    $("#slider-range-max").slider({
+    // show border width to uesr as a number
+    $("#amount-border").val($("#slider-range-border").slider("value"));
+
+    //  change border width
+    $("#slider-range-border").slider({
         change: function(event, ui) {
-            var borderWidth = $("#slider-range-max").slider("value") + 'px';
+            var borderWidth = $("#slider-range-border").slider("value") + 'px';
             $('#top_section_right>button').css({
                 'border-width': borderWidth
             });
         }
     });
 
+
+    // open color picker window to set bg-color
     $("#bg_color").click(function() {
-    	changCSS = 1;
+    	changeColor_Flag = 1;
         $("#dialog").dialog("open");
     });
 
-     $("#border_color").click(function() {
-    	changCSS = 2;
+    // open color picker window to set border-color
+    $("#border_color").click(function() {
+    	changeColor_Flag = 2;
         $("#dialog").dialog("open");
+    });
+
+    $("#set_text").click(function() {
+        $("#myByDefault").text($("#buttonValue").val());
     });
 
 
 
 
-
+     // default color in color Picker Window
     $("#red, #green, #blue").slider({
         orientation: "horizontal",
         range: "min",
@@ -56,12 +69,30 @@ $(function() {
     });
     $("#red").slider("value", 63);
     $("#green").slider("value", 124);
-    $("#blue").slider("value", 222); //3F7CDE -- 63 -- 124 -- 222 
+    $("#blue").slider("value", 222); 
 });
 
 
 
 
+
+// show choosed color in div Swatch
+function refreshSwatch() {
+    var red = $("#red").slider("value"),
+        green = $("#green").slider("value"),
+        blue = $("#blue").slider("value"),
+        hex = hexFromRGB(red, green, blue);
+    $("#swatch").css("background-color", "#" + hex);
+
+    if(changeColor_Flag == 1)
+    	$("#myByDefault").css("background-color", "#" + hex);
+    else if(changeColor_Flag == 2)
+    	$("#myByDefault").css("border-color", "#" + hex);
+    $("#put_color").val(hex);
+}
+
+
+// no need remark
 function hexFromRGB(r, g, b) {
     var hex = [
         r.toString(16),
@@ -76,20 +107,8 @@ function hexFromRGB(r, g, b) {
     return hex.join("").toUpperCase();
 }
 
-function refreshSwatch() {
-    var red = $("#red").slider("value"),
-        green = $("#green").slider("value"),
-        blue = $("#blue").slider("value"),
-        hex = hexFromRGB(red, green, blue);
-    $("#swatch").css("background-color", "#" + hex);
 
-    if(changCSS == 1)
-    	$("#myByDefault").css("background-color", "#" + hex);
-    else if(changCSS == 2)
-    	$("#myByDefault").css("border-color", "#" + hex);
-    $("#put_color").val(hex);
-}
-
+// no need remark
 function hexToRgb(hex) {
     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
